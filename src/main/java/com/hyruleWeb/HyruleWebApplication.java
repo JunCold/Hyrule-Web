1b1da559f5a159714fb5cdae657a62683ddcda71
JunCode <109869846+JunCold@users.noreply.github.com>
2023-05-31 17:29:41 -0600

    Creacion del Proyecto Inicial

---
+++ src/main/java/com/hyruleWeb/HyruleWebApplication.java
@@ -0,0 +1,13 @@
+package com.hyruleWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyruleWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyruleWebApplication.class, args);
+	}
+
+}
